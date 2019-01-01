d695e27f72a0848a8670e3979b129e6492b24c5b
AlexDoykov <doykov70@gmail.com>
2019-01-01 20:52:13 +0200

    exit feature for the chat

--- com/fmi/mpr/hw/chat/UDPClient.java
+++ com/fmi/mpr/hw/chat/UDPClient.java
@@ -12,6 +12,7 @@ class UDPClient{
 	private static InetAddress IPAddress;
 	private final static String IP = "224.0.0.3";
 	private final static int BUFF_SIZE = 8192;
+	static volatile boolean finished = false; 
 	
 	
 	private static Message deserialize(DatagramPacket request) throws IOException, ClassNotFoundException {
@@ -24,33 +25,38 @@ class UDPClient{
 	
 	public static void main(String args[]) throws IOException{
 
-		MulticastSocket clientSocket = new MulticastSocket(PORT);
-		IPAddress = InetAddress.getByName(IP);
-		clientSocket.joinGroup(IPAddress);
-		
-		readFrom = new Scanner(System.in);
-		
-		System.out.print("Please choose your username: ");
-		username = readFrom.nextLine();
-		
-		Thread thread = new Thread(new WriteThread(readFrom, username, IPAddress, PORT, clientSocket));
-		thread.start();	
-		
-		while(true) {
+		try(MulticastSocket clientSocket = new MulticastSocket(PORT)){
+			IPAddress = InetAddress.getByName(IP);
+			clientSocket.joinGroup(IPAddress);
+			
+			readFrom = new Scanner(System.in);
+			System.out.println("Enter q to exit!");
+			System.out.print("Please choose your username: ");
+			username = readFrom.nextLine();
+			
+			Thread thread = new Thread(new WriteThread(readFrom, username, IPAddress, PORT, clientSocket));
+			thread.start();	
+			
 			byte[] receiveData = new byte[BUFF_SIZE];
 			DatagramPacket receivedPacket = new DatagramPacket(receiveData, receiveData.length);
-			while(true) {
+			while(!finished) {
 				clientSocket.receive(receivedPacket);
 				Message receivedMessage;
 				try {
 					receivedMessage = deserialize(receivedPacket);
+					String toShow = new String(receivedMessage.getContent());
 					if(!receivedMessage.getName().equals(username)) {
-						if(receivedMessage.getType() == Type.TEXT) {
-							String toShow = new String(receivedMessage.getContent());
+						if(receivedMessage.getType() == Type.TEXT && !toShow.equals("q")) {
 							System.out.println(receivedMessage.getName() + " writes: " + toShow);
 						}else {
 							save(receivedMessage, receivedMessage.getType());
 						}
+					}else {
+						if(toShow.equals("q")) {
+							finished = true;
+	                        clientSocket.leaveGroup(IPAddress); 
+							continue;
+						}						
 					}
 				} catch (ClassNotFoundException e) {
 					// TODO Auto-generated catch block
@@ -59,7 +65,7 @@ class UDPClient{
 			}
 		}
 				
-		}
+	}
 
 	private static void save(Message message, Type type) {
 		String fileToWrite = message.getFilename() + "." + message.getFileExtension();
